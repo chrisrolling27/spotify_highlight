d61e25db48061ef1e47aa4dc1ba11840606c33df
chris <chrisrolling27@gmail.com>
2023-06-10 14:41:41 -0700

    random string

--- server/index.js
+++ server/index.js
@@ -10,13 +10,28 @@ app.use(express.json());
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
+const stateKey = "spotify_auth_state";
+
+const generateRandomString = (length) => {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+app.get("/login", (req, res) => {
+  const queryParams = querystring.stringify({
+    client_id: CLIENT_ID,
+    response_type: "code",
+    redirect_uri: REDIRECT_URI,
+  });
+
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+});
 
 app.listen(port, () => {
   console.log(`server listening to http://localhost:${port}`);
-});
-
-app.get("/login", (req, res) => {
-  res.redirect(
-    `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`
-  );
 });
