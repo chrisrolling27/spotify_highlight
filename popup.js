dc1c1ad47fc8d718a3b9a5f8e4c4ca3773564824
chris <chrisrolling27@gmail.com>
2023-07-03 17:05:12 -0700

    spotify pinger

--- popup.js
+++ popup.js
@@ -1,55 +1,48 @@
 let port = chrome.runtime.connect({ name: "popup" });
 
-//test highlighter
-// port.onMessage.addListener((message) => {
-//     document.body.innerHTML = message.text;
-// });
+chrome.storage.local.set({ accessToken: "xxx" });
 
-//spotify test button for pinging spotify on example endpoint instead of waiting for message
+//spotify /example pinger 
 document.addEventListener("DOMContentLoaded", () => {
   const spotifyButton = document.getElementById("spotify-button");
+  const trackContainer = document.getElementById("track-container");
 
   spotifyButton.addEventListener("click", () => {
-    const exampleUrl = "http://localhost:3000/example";
+    // Fetch the "name" from chrome.storage.local
+    chrome.storage.local.get(["accessToken"], (result) => {
 
-    // Get access_token from Chrome Storage API
-    chrome.storage.local.get("access_token", (result) => {
-      const accessToken = result.access_token;
+      const accessToken = result.accessToken;
+      const exampleURL = "http://localhost:3000/example";
 
-      if (!accessToken) {
-        console.log("No access token found");
-        return;
-      }
-
-      // Make a request to the /example endpoint
-      fetch(exampleUrl, {
+      // Fetch data from the exampleURL
+      fetch(exampleURL, {
         headers: {
-          Authorization: `Bearer ${accessToken}`,
+          "access": accessToken,
         },
       })
         .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
+          // Parse the response as JSON
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("Failed to load data");
           }
-          return response.json();
         })
         .then((data) => {
-          // Process your data here
-          console.log(data);
-
-          // For example, you can display it in your popup
-          const displayElement = document.getElementById("track-container"); // Assume you have an element with id 'track-container' to show the data
-          displayElement.textContent = JSON.stringify(data, null, 2);
+          // Use the JSON data to display in the trackContainer
+          trackContainer.innerHTML = JSON.stringify(data, null, 2);
         })
-        .catch((error) =>
-          console.error(
-            "There has been a problem with your fetch operation:",
-            error
-          )
-        );
+        .catch((error) => {
+          console.error("Error fetching data: ", error);
+          trackContainer.innerHTML = "Failed to load data";
+        });
     });
   });
 });
+
+
+
+
 
 //real listener that takes message from highlighted text to the backend.
 // port.onMessage.addListener((message) => {
@@ -93,42 +86,7 @@ document.addEventListener("DOMContentLoaded", () => {
 //     });
 // });
 
-chrome.storage.local.set({ accessToken: "xxx" });
-
-//popup.html button
-document.addEventListener("DOMContentLoaded", () => {
-  const spotifyButton = document.getElementById("spotify-button");
-  const trackContainer = document.getElementById("track-container");
-
-  spotifyButton.addEventListener("click", () => {
-    // Fetch the "name" from chrome.storage.local
-    chrome.storage.local.get(["accessToken"], (result) => {
-
-      const accessToken = result.accessToken;
-      const exampleURL = "http://localhost:3000/example";
-
-      // Fetch data from the exampleURL
-      fetch(exampleURL, {
-        headers: {
-          "access": accessToken,
-        },
-      })
-        .then((response) => {
-          // Parse the response as JSON
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error("Failed to load data");
-          }
-        })
-        .then((data) => {
-          // Use the JSON data to display in the trackContainer
-          trackContainer.innerHTML = JSON.stringify(data, null, 2);
-        })
-        .catch((error) => {
-          console.error("Error fetching data: ", error);
-          trackContainer.innerHTML = "Failed to load data";
-        });
-    });
-  });
-});
+//test highlighter
+// port.onMessage.addListener((message) => {
+//     document.body.innerHTML = message.text;
+// });
