3dda66163a2b67cb3b148903877889dd25b3071e
chris <chrisrolling27@gmail.com>
2023-09-06 17:31:56 -0700

    latest

--- background.js
+++ background.js
@@ -26,6 +26,46 @@ chrome.runtime.onConnect.addListener((port) => {
   port.postMessage({ text: selectedText });
 });
 
+//new
+chrome.runtime.onInstalled.addListener(function() {
+  getSpotifyToken();
+});
+
+function getSpotifyToken() {
+  const CLIENT_ID = process.env.CLIENT_ID;
+  const redirectUri = chrome.identity.getRedirectURL('callback');
+  const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=token&scope=playlist-read-private`;
+
+  chrome.identity.launchWebAuthFlow(
+    {
+      url: authUrl,
+      interactive: true,
+    },
+    function (redirectedTo) {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+        return;
+      }
+      
+      // Extract token from the URL
+      const url = new URL(redirectedTo);
+      const hash = url.hash.substring(1);
+
+      const params = new URLSearchParams(hash);
+      const accessToken = params.get('access_token');
+
+      // Use access_token to make Spotify API calls
+      // Store it or use it for immediate API calls
+      console.log(`Received access token: ${accessToken}`);
+    }
+  );
+}
+
+
+
+
+
+
 
 // const fetchToken = () => {
 //   const url = "localhost:3000/callback
